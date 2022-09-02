1191ef8747ffa3b5f4643aba4aaad77482766aad
Artur-Niezgoda <110778550+Artur-Niezgoda@users.noreply.github.com>
2022-09-02 14:29:13 +0200

    add basic functionality

--- main.py
+++ main.py
@@ -1,8 +1,49 @@
 from tkinter import *
+from tkinter import messagebox
+from random import randint, shuffle, choice
+import pyperclip
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    password_list = [choice(letters) for _ in range(randint(8, 10))] + \
+                    [choice(symbols) for _ in range(randint(2, 4))] + \
+                    [choice(numbers) for _ in range(randint(2, 4))]
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save_password():
+    website = web_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    if website == "" or email == "" or password == "":
+        messagebox.showinfo(title="Empty Fields", message="One or more of the fields remain empty. Please fill all "
+                                                          "the fields")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"There are the details entered: \n "
+                                                              f"Email: {email} \n"
+                                                              f"Password: {password} \n")
+        if is_ok:
+            with open("password.txt", "a") as file:
+                file.write(f"{website} | {email} | {password} \n")
+            web_entry.delete(0, END)
+            email_entry.delete(0, END)
+            email_entry.insert(0, "example@email.com")
+            password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -31,15 +72,17 @@ password_label.grid(row=3, column=0)
 # Entries
 web_entry = Entry(mainframe)
 web_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+web_entry.focus()
 email_entry = Entry(mainframe)
 email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+email_entry.insert(0, "example@email.com")
 password_entry = Entry(mainframe)
 password_entry.grid(row=3, column=1, sticky="EW")
 
 # Buttons
-button_gen_pass = Button(mainframe, text="Generate Password")
+button_gen_pass = Button(mainframe, text="Generate Password", command=generate_password)
 button_gen_pass.grid(row=3, column=2, sticky="EW")
-add_button = Button(mainframe, text="Add", width=35)
+add_button = Button(mainframe, text="Add", width=35, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 # Add weights to the grid rows and columns
